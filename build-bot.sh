31072c5eedd122531dc53e552c9236ddaf82d19c
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-07-26 12:48:22 -0400

    Build the bot

--- build-bot.sh
+++ build-bot.sh
@@ -11,18 +11,17 @@ SLOTS="CompareX CountX PrepositionX ResetX TicketsSoldX TopX VersusX cat_descX d
 for i in $SLOTS
 do
 	echo "Creating Slot Type: $i"
-	aws lex-models put-slot-type --name $i --cli-input-json file://slots/$i.json
+	aws lex-models put-slot-type --name $i --cli-input-json file://slots/$i.json >/dev/null 2>&1
 done
 
 # build the intents
 for i in $INTENTS
 do
 	echo "Creating Intent: $i"
-	aws lex-models put-intent --name $i --cli-input-json file://intents/$i.json
+	aws lex-models put-intent --name $i --cli-input-json file://intents/$i.json >/dev/null 2>&1
 done
   
 # build the bot 
-# if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' # >>/dev/null 2>&1
-# then echo "Deleting Bot: $BOT"; aws lex-models delete-bot $BOT
-# fi
+echo "Creating Bot: $i"
+aws lex-models put-bot --name $BOT --cli-input-json file://bots/$BOT.json
 
