a0ba9ada775817960f277a009ad00b1f3aec574a
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-07-26 12:12:51 -0400

    Build some slot types

---
+++ build-bot.sh
@@ -0,0 +1,29 @@
+
+#
+# Builds the bot, intents, and custom slot types
+#
+
+BOT="JasperX"
+INTENTS="CompareX CountX GoodByeX HelloX RefreshX ResetX TopX"
+# SLOTS="CompareX CountX PrepositionX ResetX TicketsSoldX TopX VersusX cat_descX dimensionsX event_nameX"
+SLOTS="CompareX CountX"
+
+# delete the custom slot types
+for i in $SLOTS
+do
+	aws lex-models put-slot-type --name $i --cli-input-json file://slots/$i.json
+done
+
+# delete the intents
+# for i in $INTENTS
+# do
+# 	if aws lex-models get-intent --name $i --intent-version '$LATEST' # >>/dev/null 2>&1
+#    	then echo "Deleting Intent: $i"; aws lex-models delete-intent --name $i
+# 	fi
+# done
+  
+# delete the bot if it exists
+# if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' # >>/dev/null 2>&1
+# then echo "Deleting Bot: $BOT"; aws lex-models delete-bot $BOT
+# fi
+
