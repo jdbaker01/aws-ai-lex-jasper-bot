9ffc3601c9fc29cb073ee3e4e7846bb9b46e83b2
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-07-26 12:31:41 -0400

    Build some intents

--- build-bot.sh
+++ build-bot.sh
@@ -4,25 +4,25 @@
 #
 
 BOT="JasperX"
-INTENTS="CompareX CountX GoodByeX HelloX RefreshX ResetX TopX"
+# INTENTS="CompareX CountX GoodByeX HelloX RefreshX ResetX TopX"
+INTENTS="CompareX"
 SLOTS="CompareX CountX PrepositionX ResetX TicketsSoldX TopX VersusX cat_descX dimensionsX event_nameX"
 
-# delete the custom slot types
+# build the custom slot types
 for i in $SLOTS
 do
 	echo "Creating Slot Type: $i"
 	aws lex-models put-slot-type --name $i --cli-input-json file://slots/$i.json
 done
 
-# delete the intents
-# for i in $INTENTS
-# do
-# 	if aws lex-models get-intent --name $i --intent-version '$LATEST' # >>/dev/null 2>&1
-#    	then echo "Deleting Intent: $i"; aws lex-models delete-intent --name $i
-# 	fi
-# done
+# build the intents
+for i in $INTENTS
+do
+	echo "Creating Intent: $i"
+	aws lex-models put-intent --name $i --cli-input-json file://intents/$i.json
+done
   
-# delete the bot if it exists
+# build the bot 
 # if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' # >>/dev/null 2>&1
 # then echo "Deleting Bot: $BOT"; aws lex-models delete-bot $BOT
 # fi
--- delete-bot.sh
+++ delete-bot.sh
@@ -10,14 +10,14 @@ INTENTS="CompareX CountX GoodByeX HelloX RefreshX ResetX TopX"
 SLOTS="CompareX CountX PrepositionX ResetX TicketsSoldX TopX VersusX cat_descX dimensionsX event_nameX"
 
 # delete the bot if it exists
-if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' # >>/dev/null 2>&1
+if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' >>/dev/null 2>&1
 then echo "Deleting Bot: $BOT"; aws lex-models delete-bot $BOT
 fi
 
 # delete the intents
 for i in $INTENTS
 do
-	if aws lex-models get-intent --name $i --intent-version '$LATEST' # >>/dev/null 2>&1
+	if aws lex-models get-intent --name $i --intent-version '$LATEST' >>/dev/null 2>&1
    	then echo "Deleting Intent: $i"; aws lex-models delete-intent --name $i
 	fi
 done
@@ -25,7 +25,7 @@ done
 # delete the custom slot types
 for i in $SLOTS
 do
-	if aws lex-models get-slot-type --name $i --slot-type-version '$LATEST' # >>/dev/null 2>&1
+	if aws lex-models get-slot-type --name $i --slot-type-version '$LATEST' >>/dev/null 2>&1
    	then echo "Deleting Slot Type: $i"; aws lex-models delete-slot-type --name $i
 	fi
 done
