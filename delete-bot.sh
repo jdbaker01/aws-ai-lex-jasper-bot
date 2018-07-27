f441327268a57cd478a8d0a8c84193d1873065f7
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-07-27 06:37:27 -0400

    Working on conflicting resource use exceptions

--- delete-bot.sh
+++ delete-bot.sh
@@ -9,29 +9,50 @@ BOT="JasperX"
 INTENTS="CompareX CountX GoodByeX HelloX RefreshX ResetX TopX"
 SLOTS="CompareX CountX PrepositionX ResetX TicketsSoldX TopX VersusX cat_descX dimensionsX event_nameX"
 LAMBDA="JasperX"
+SLEEP=2
 
 # delete the bot if it exists
 if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' >/dev/null 2>&1
-then echo "Deleting Bot: $BOT"; aws lex-models delete-bot --name $BOT; sleep 10
+then 
+    echo "sleep $SLEEP..."
+    echo "Deleting Bot: $BOT"
+    aws lex-models delete-bot --name $BOT
+    echo "sleep $SLEEP..."
+    sleep $SLEEP
 fi
 
 # delete the intents
 for i in $INTENTS
 do
-	if aws lex-models get-intent --name $i --intent-version '$LATEST' >/dev/null 2>&1
-   	then echo "Deleting Intent: $i"; aws lex-models delete-intent --name $i; sleep 10
-	fi
+    if aws lex-models get-intent --name $i --intent-version '$LATEST' >/dev/null 2>&1
+    then 
+        echo "sleep $SLEEP..."
+        echo "Deleting Intent: $i"
+        aws lex-models delete-intent --name $i
+        echo "sleep $SLEEP..."
+        sleep $SLEEP
+    fi
 done
 
 # delete the custom slot types
 for i in $SLOTS
 do
-	if aws lex-models get-slot-type --name $i --slot-type-version '$LATEST' >/dev/null 2>&1
-   	then echo "Deleting Slot Type: $i"; aws lex-models delete-slot-type --name $i; sleep 10
-	fi
+    if aws lex-models get-slot-type --name $i --slot-type-version '$LATEST' >/dev/null 2>&1
+    then 
+        echo "sleep $SLEEP..."
+        echo "Deleting Slot Type: $i"
+        aws lex-models delete-slot-type --name $i
+        echo "sleep $SLEEP..."
+        sleep $SLEEP
+    fi
 done
  
 # delete the lambda function
 if aws lambda get-function --function-name $LAMBDA >/dev/null 2>&1
-then echo "Deleting Lambda Function: $LAMBDA"; aws lambda delete-function --function-name $LAMBDA; sleep 10
+then 
+    echo "sleep $SLEEP..."
+    echo "Deleting Lambda Function: $LAMBDA"
+    aws lambda delete-function --function-name $LAMBDA
+    echo "sleep $SLEEP..."
+    sleep $SLEEP
 fi
