c5b2c29672abc3c8b347ef19442bd4cf7689d366
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-07-26 22:11:32 -0400

    Debugging

--- build-bot.sh
+++ build-bot.sh
@@ -11,18 +11,18 @@ SLOTS="CompareX CountX PrepositionX ResetX TicketsSoldX TopX VersusX cat_descX d
 for i in $SLOTS
 do
 	echo "Creating Slot Type: $i"
-	aws lex-models put-slot-type --name $i --cli-input-json file://slots/$i.json >/dev/null 2>&1
+	aws lex-models put-slot-type --name $i --cli-input-json file://slots/$i.json >/dev/null 
 done
 
 # build the intents
 for i in $INTENTS
 do
 	echo "Creating Intent: $i"
-	aws lex-models put-intent --name $i --cli-input-json file://intents/$i.json >/dev/null 2>&1
+	aws lex-models put-intent --name $i --cli-input-json file://intents/$i.json >/dev/null 
 done
   
 # deploy the Lambda intent handler
-aws lambda create-function --function-name JasperX --description 'JasperX Bot - Tickit database' --timeout 300 --zip-file fileb://JasperLambda.zip --role arn:aws:iam::687551564203:role/LambdaServiceRoleAthenaS3 --handler lambda_function.lambda_handler --runtime python3.6 --profile adminuser 
+aws lambda create-function --function-name JasperX --description 'JasperX Bot - Tickit database' --timeout 300 --zip-file fileb://JasperLambda.zip --role arn:aws:iam::687551564203:role/LambdaServiceRoleAthenaS3 --handler lambda_function.lambda_handler --runtime python3.6 
 
 # build the bot 
 echo "Creating Bot: $BOT"
