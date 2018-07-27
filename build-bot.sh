2353e838d880e57cb22963eaf8b475055cab4afc
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-07-27 06:48:39 -0400

    Tweaking sleeps and output logging

--- build-bot.sh
+++ build-bot.sh
@@ -11,9 +11,11 @@ ATHENA_DB="tickit"
 ATHENA_OUTPUT_LOCATION="s3://ai-aod-demo-bryost-athena-output"
 
 # deploy the Lambda intent handler
-aws lambda create-function --function-name $LAMBDA --description "$LAMBDA Intent Handler" --timeout 300 --zip-file fileb://JasperLambda.zip --role arn:aws:iam::687551564203:role/LambdaServiceRoleAthenaS3 --handler lambda_function.lambda_handler --runtime python3.6 --environment "Variables={ATHENA_DB=$ATHENA_DB,ATHENA_OUTPUT_LOCATION=$ATHENA_OUTPUT_LOCATION}"
+echo "Creating Lambda Handler: $LAMBDA"
+aws lambda create-function --function-name $LAMBDA --description "$LAMBDA Intent Handler" --timeout 300 --zip-file fileb://JasperLambda.zip --role arn:aws:iam::687551564203:role/LambdaServiceRoleAthenaS3 --handler lambda_function.lambda_handler --runtime python3.6 --environment "Variables={ATHENA_DB=$ATHENA_DB,ATHENA_OUTPUT_LOCATION=$ATHENA_OUTPUT_LOCATION}" >/dev/null
 
-aws lambda add-permission --function-name $LAMBDA --statement-id chatbot-fulfillment --action "lambda:InvokeFunction" --principal "lex.amazonaws.com"
+echo "Adding permission to invoke Lambda Handler: $LAMBDA from Amazon Lex"
+aws lambda add-permission --function-name $LAMBDA --statement-id chatbot-fulfillment --action "lambda:InvokeFunction" --principal "lex.amazonaws.com" >/dev/null
 
 # build the custom slot types
 for i in $SLOTS
--- delete-bot.sh
+++ delete-bot.sh
@@ -12,51 +12,51 @@ LAMBDA="JasperX"
 SLEEP=2
 
 # delete the bot if it exists
-if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' >/dev/null 2>&1
+if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' >/dev/null
 then 
     # echo "sleep $SLEEP..."
     sleep $SLEEP
     echo "Deleting Bot: $BOT"
     aws lex-models delete-bot --name $BOT
     # echo "sleep $SLEEP..."
-    # sleep $SLEEP
+    sleep $SLEEP
 fi
 
 # delete the intents
 for i in $INTENTS
 do
-    if aws lex-models get-intent --name $i --intent-version '$LATEST' >/dev/null 2>&1
+    if aws lex-models get-intent --name $i --intent-version '$LATEST' >/dev/null
     then 
         # echo "sleep $SLEEP..."
         sleep $SLEEP
         echo "Deleting Intent: $i"
         aws lex-models delete-intent --name $i
         # echo "sleep $SLEEP..."
-        # sleep $SLEEP
+        sleep $SLEEP
     fi
 done
 
 # delete the custom slot types
 for i in $SLOTS
 do
-    if aws lex-models get-slot-type --name $i --slot-type-version '$LATEST' >/dev/null 2>&1
+    if aws lex-models get-slot-type --name $i --slot-type-version '$LATEST' >/dev/null
     then 
         # echo "sleep $SLEEP..."
         sleep $SLEEP
         echo "Deleting Slot Type: $i"
         aws lex-models delete-slot-type --name $i
         # echo "sleep $SLEEP..."
-        # sleep $SLEEP
+        sleep $SLEEP
     fi
 done
  
 # delete the lambda function
-if aws lambda get-function --function-name $LAMBDA >/dev/null 2>&1
+if aws lambda get-function --function-name $LAMBDA >/dev/null
 then 
     # echo "sleep $SLEEP..."
     sleep $SLEEP
     echo "Deleting Lambda Function: $LAMBDA"
     aws lambda delete-function --function-name $LAMBDA
     # echo "sleep $SLEEP..."
-    # sleep $SLEEP
+    sleep $SLEEP
 fi
