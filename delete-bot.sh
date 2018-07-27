89cd933496f8c66b7a42cc4f9b6e40396bfd0106
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-07-27 07:57:46 -0400

    Clean up excess logging (echo sleep...)

--- delete-bot.sh
+++ delete-bot.sh
@@ -14,11 +14,9 @@ SLEEP=3
 # delete the bot if it exists
 if aws lex-models get-bot --name $BOT --version-or-alias '$LATEST' >/dev/null
 then 
-    # echo "sleep $SLEEP..."
     sleep $SLEEP
     echo "Deleting Bot: $BOT"
     aws lex-models delete-bot --name $BOT
-    # echo "sleep $SLEEP..."
     sleep $SLEEP
 fi
 
@@ -27,11 +25,9 @@ for i in $INTENTS
 do
     if aws lex-models get-intent --name $i --intent-version '$LATEST' >/dev/null
     then 
-        # echo "sleep $SLEEP..."
         sleep $SLEEP
         echo "Deleting Intent: $i"
         aws lex-models delete-intent --name $i
-        # echo "sleep $SLEEP..."
         sleep $SLEEP
     fi
 done
@@ -41,11 +37,9 @@ for i in $SLOTS
 do
     if aws lex-models get-slot-type --name $i --slot-type-version '$LATEST' >/dev/null
     then 
-        # echo "sleep $SLEEP..."
         sleep $SLEEP
         echo "Deleting Slot Type: $i"
         aws lex-models delete-slot-type --name $i
-        # echo "sleep $SLEEP..."
         sleep $SLEEP
     fi
 done
@@ -53,10 +47,8 @@ done
 # delete the lambda function
 if aws lambda get-function --function-name $LAMBDA >/dev/null
 then 
-    # echo "sleep $SLEEP..."
     sleep $SLEEP
     echo "Deleting Lambda handler function: $LAMBDA"
     aws lambda delete-function --function-name $LAMBDA
-    # echo "sleep $SLEEP..."
     sleep $SLEEP
 fi
