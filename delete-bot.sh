a9c4bee7786b1d24f11930d44e1a21ee51234482
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-08-14 12:31:58 -0400

    Moving environment variables to CloudFormation

--- delete-bot.sh
+++ delete-bot.sh
@@ -6,11 +6,16 @@
 # the intents, then the slot types.
 #
 
-BOT="JasperX"
-ALIAS="jasper_bot"
-INTENTS="CompareX CountX GoodByeX HelloX RefreshX ResetX TopX"
-SLOTS="CompareX CountX PrepositionX ResetX TicketsSoldX TopX VersusX cat_descX dimensionsX event_nameX"
-# LAMBDA variable set by CodeBuild
+#
+# Environment variables to be set in the CodeBuild project
+#
+# $BOT				Name of the Lex bot
+# $ALIAS			Name of the published alias for the bot
+# $INTENTS      		List of intent names for the bot
+# $SLOTS        		List of slot type names for the bot
+# $LAMBDA       		Name of the Lambda fulfillment function for the bot
+#
+
 SLEEP=2
 
 # delete bot aliases -- fix this
