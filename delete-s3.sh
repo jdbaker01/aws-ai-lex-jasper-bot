532f1e5e293c3a5363912c52ea363e8d47dd1580
Yost <bryost@8c8590b9ca09.ant.amazon.com>
2018-08-24 09:16:03 -0400

    Deleting S3 buckets

--- delete-s3.sh
+++ delete-s3.sh
@@ -11,7 +11,11 @@
 #
 # $ATHENA_BUCKET		S3 bucket where data files reside
 # $ATHENA_OUTPUT_LOCATION	S3 bucket for Athena output 
+# $ARTIFACT_STIRE		S3 bucket for CodePipeline / CodeBuild code sharing
 #
+
+ATHENA_BUCKET=s3://jasper-athenabucket-18gefpj6x7rws
+ATHENA_OUTPUT_LOCATION=s3://jasper-athenaoutputlocation-eqn5yl8v7azh
 
 echo "Deleting S3 bucket $ATHENA_BUCKET."
 aws s3 rm --recursive $ATHENA_BUCKET
@@ -19,4 +23,6 @@ aws s3 rm --recursive $ATHENA_BUCKET
 echo "Deleting S3 bucket $ATHENA_OUTPUT_LOCATION."
 aws s3 rm --recursive $ATHENA_OUTPUT_LOCATION
 
+echo "Deleting S3 bucket $ARTIFACT_STORE."
+aws s3 rm --recursive $ARTIFACT_STORE
 
